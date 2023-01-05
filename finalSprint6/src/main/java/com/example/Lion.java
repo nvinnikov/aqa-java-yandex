786469f9c6bd38830562548e5c1010d2503f6879
Nikita Vinnikov <nvinnikov@miem.hse.ru>
2023-01-06 02:02:04 +0600

    Sprint 6 Final

---
+++ finalSprint6/src/main/java/com/example/Lion.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.util.List;
+
+public class Lion {
+
+    private final boolean hasMane;
+    Feline feline;
+
+    public Lion(Feline feline, String sex) throws Exception {
+        this.feline = feline;
+        if ("Самец".equals(sex)) {
+            hasMane = true;
+        } else if ("Самка".equals(sex)) {
+            hasMane = false;
+        } else {
+            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+        }
+    }
+
+
+    public int getKittens() {
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
+    public List<String> getFood() throws Exception {
+        return feline.getFood("Хищник");
+    }
+}
