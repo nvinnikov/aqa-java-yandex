6ba1260dc42742d4d07d5dfa92549ac27702e9e7
Nikita Vinnikov <nvinnikov@miem.hse.ru>
2023-01-11 06:48:51 +0300

    develop1

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -3,9 +3,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import praktikum.Database;
+import praktikum.*;
 
 public class BurgerTest {
+    Bun bun = new Bun("СмолТести", (float) 111.2);
     @Mock
     Database database;
 
@@ -13,8 +14,33 @@ public class BurgerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
+    @Test
+    public void setBunsTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        Assert.assertEquals(burger.bun, bun);
+    }
+//    @Test
+//    public void addIngredientTest(){
+//        Burger burger = new Burger();
+//
+//        Assert.assertEquals();
+//    }
+//    @Test
+//    public void removeIngredientTest(){
+//        Assert.assertEquals();
+//    }
+//    @Test
+//    public void moveIngredientTest(){
+//        Assert.assertEquals();
+//    }
 //    @Test
 //    public void getPriceTest(){
 //        Assert.assertEquals();
 //    }
+//    @Test
+//    public void getReceiptTest(){
+//        Assert.assertEquals();
+//    }
+
 }
