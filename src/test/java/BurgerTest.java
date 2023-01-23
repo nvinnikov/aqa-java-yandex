91769c24317d1b186a334e0de1fd6b70c7e9bea7
Nikita Vinnikov <nvinnikov@miem.hse.ru>
2023-01-23 10:00:26 +0300

    upd

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,48 +1,48 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.Mockito;
 import org.junit.runner.RunWith;
-import praktikum.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-    @Mock
-    Database database;
 
     @Test
-    public void setBunsTest(){
+    public void setBunsTest() {
         Burger burger = new Burger();
         Bun bun = new Bun("СмолТести", (float) 111.2);
         burger.setBuns(bun);
-        Assert.assertEquals(burger.bun, bun);
+        Assert.assertEquals(bun, burger.bun);
     }
+
     @Test
-    public void addIngredientTest(){
+    public void addIngredientTest() {
         Burger burger = new Burger();
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Лук", (float) 20.0);
         List<Ingredient> ingredients = new ArrayList<>();
         ingredients.add(ingredient);
         burger.addIngredient(ingredient);
-        Assert.assertEquals(burger.ingredients, ingredients);
+        Assert.assertEquals(ingredients, burger.ingredients);
     }
+
     @Test
-    public void removeIngredientTest(){
+    public void removeIngredientTest() {
         Burger burger = new Burger();
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Лук", (float) 20.0);
         List<Ingredient> ingredients = new ArrayList<>();
         burger.addIngredient(ingredient);
         burger.removeIngredient(0);
-        Assert.assertEquals(burger.ingredients, ingredients);
+        Assert.assertEquals(ingredients, burger.ingredients);
     }
+
     @Test
-    public void moveIngredientTest(){
+    public void moveIngredientTest() {
         Burger burger = new Burger();
         Ingredient ingredient0 = new Ingredient(IngredientType.SAUCE, "Лук", (float) 20.0);
         Ingredient ingredient1 = new Ingredient(IngredientType.FILLING, "Помидор", (float) 50.0);
@@ -51,20 +51,22 @@ public class BurgerTest {
         ingredients.add(ingredient0);
         burger.addIngredient(ingredient0);
         burger.addIngredient(ingredient1);
-        burger.moveIngredient(0,1);
-        Assert.assertEquals(burger.ingredients, ingredients);
+        burger.moveIngredient(0, 1);
+        Assert.assertEquals(ingredients, burger.ingredients);
     }
+
     @Test
-    public void getPriceTest(){
+    public void getPriceTest() {
         Burger burger = new Burger();
         Bun bun = new Bun("СмолТести", (float) 111.2);
         burger.setBuns(bun);
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Лук", (float) 20.0);
         burger.addIngredient(ingredient);
-        Assert.assertEquals(burger.getPrice(), (float) 242.4, 0.01);
+        Assert.assertEquals((float) 242.4, burger.getPrice(), 0.01);
     }
+
     @Test
-    public void getReceiptTest(){
+    public void getReceiptTest() {
         Burger burger = new Burger();
         Bun bun = new Bun("СмолТести", (float) 111.2);
         burger.setBuns(bun);
@@ -79,7 +81,7 @@ public class BurgerTest {
         }
         receipt.append(String.format("(==== %s ====)%n", bun.getName()));
         receipt.append(String.format("%nPrice: %f%n", (float) 242.4));
-        Assert.assertEquals(burger.getReceipt(), receipt.toString());
+        Assert.assertEquals(receipt.toString(), burger.getReceipt());
     }
 
 }
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,9 +1,5 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import praktikum.Database;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
@@ -11,17 +7,19 @@ public class IngredientTest {
     private IngredientType ingredientType;
 
     @Test
-    public void getPriceTest(){
+    public void getPriceTest() {
         Ingredient ingredient = new Ingredient(ingredientType, "qwe", (float) 2.2);
         Assert.assertEquals((float) 2.2, ingredient.getPrice(), 0.01);
     }
+
     @Test
-    public void getNameTest(){
+    public void getNameTest() {
         Ingredient ingredient = new Ingredient(ingredientType, "qwe", (float) 2.2);
         Assert.assertEquals("qwe", ingredient.getName());
     }
+
     @Test
-    public void getTypeTest(){
+    public void getTypeTest() {
         Ingredient ingredient = new Ingredient(ingredientType, "qwe", (float) 2.2);
         Assert.assertEquals(ingredientType, ingredient.getType());
     }
